8c84a4d139d51b05f4ac245c582a7a91c304f36a
Ebenclaw <rebekasanper15@gmail.com>
2023-11-17 10:50:34 +0100

    Version1.0

---
+++ indexProyectoDIW.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="webroot/css/style.css">
+    <link rel="icon" type="image/x-icon" href="/webroot/image/flora.png">
+    <title>Rebeca Sánchez Pérez</title>
+</head>
+
+<body>
+    <header>
+        <a href="../index.html"><img id="logo" src="webroot/image/logo.png" alt="Logo"></a>
+        <h1>Diseño de Interfaces Web - DIW</h1>
+    </header>
+    <main>
+        <table>
+            <caption>Prácticas</caption>
+            <tr>
+                <td>1-1-1 - El Color</td>
+                <td><a href="practicas/pr-1-1-1.html" target="_blank"><img src="webroot/image/play.png" alt="play"/></a></td>
+                <td><a href="doc/1-1-1.pdf" target="_blank"><img src="webroot/image/ojo.png" alt="ojo"/></a></td>
+            </tr>
+            <tr>
+                <td>1-1-2 - Color: Obtener el complementario</td>
+                <td><a href="practicas/pr-1-1-2.html" target="_blank"><img src="webroot/image/play.png" alt="play"/></a></td>
+                <td><a href="doc/1-1-2.pdf" target="_blank"><img src="webroot/image/ojo.png" alt="ojo"/></a></td>
+            </tr>
+            <tr>
+                <td>1-2-1 - Tipografía</td>
+                <td><a href="practicas/pr-1-2-1.html" target="_blank"><img src="webroot/image/play.png" alt="play"/></a></td>
+                <td><a href="doc/1-2-1.pdf" target="_blank"><img src="webroot/image/ojo.png" alt="ojo"/></a></td>
+            </tr>
+            <tr>
+                <td>1-2-3 - Tablas</td>
+                <td><a href="practicas/pr-1-2-3.html" target="_blank"><img src="webroot/image/play.png" alt="play"/></a></td>
+                <td><a href="doc/1-2-3.pdf" target="_blank"><img src="webroot/image/ojo.png" alt="ojo"/></a></td>
+            </tr>
+            <tr>
+                <td>2-2 - Etiquetas HTML5 + selectores css</td>
+                <td><a href="practicas/pr-2-2.html" target="_blank"><img src="webroot/image/play.png" alt="play"/></a></td>
+                <td><a href="doc/2-2.pdf" target="_blank"><img src="webroot/image/ojo.png" alt="ojo"/></a></td>
+            </tr>
+            <tr>
+                <td>2-3 - Clases en listas</td>
+                <td><a href="practicas/pr-2-3.html" target="_blank"><img src="webroot/image/play.png" alt="play"/></a></td>
+                <td><a href="doc/2-3.pdf" target="_blank"><img src="webroot/image/ojo.png" alt="ojo"/></a></td>
+            </tr>
+            <tr>
+                <td>2-4-1 - Selectores CSS: Ejercicio 1</td>
+                <td><a href="practicas/pr-2-4-1.html" target="_blank"><img src="webroot/image/play.png" alt="play"/></a></td>
+                <td><a href="doc/2-4.pdf" target="_blank"><img src="webroot/image/ojo.png" alt="ojo"/></a></td>
+            </tr>
+            <tr>
+                <td>2-4-2 - Selectores CSS: Ejercicio 2</td>
+                <td><a href="practicas/pr-2-4-2.html" target="_blank"><img src="webroot/image/play.png" alt="play"/></a></td>
+                <td><a href="doc/2-4.pdf" target="_blank"><img src="webroot/image/ojo.png" alt="ojo"/></a></td>
+            </tr>
+            <tr>
+                <td>2-4-3 - Selectores CSS: Ejercicio 3</td>
+                <td><a href="practicas/pr-2-4-3.html" target="_blank"><img src="webroot/image/play.png" alt="play"/></td>
+                <td><a href="doc/2-4.pdf" target="_blank"><img src="webroot/image/ojo.png" alt="ojo"/></a></td>
+            </tr>
+            <tr>
+                <td>2-5 - Pseudo-clases y formularios</td>
+                <td></td>
+                <td><a href="doc/2-5.pdf" target="_blank"><img src="webroot/image/ojo.png" alt="ojo"/></a></td>
+            </tr>
+        </table>
+    </main>
+    <footer>
+        <div id="derechos">2023-2024 © Todos los derechos reservados: <a href="../index.html">Rebeca Sánchez Pérez</a></div>
+        <div id="fotos">
+            <a href="https://github.com/Ebenclaw" target="_blank"><img id="git" src="webroot/image/GitHub.png" alt="GitHub"></a>
+            <a href="http://ieslossauces.centros.educa.jcyl.es/sitio/" target="_blank"><img id="sauces" src="webroot/image/sauces.png" alt="Sauces"></a>
+            <a href="../index.html"><img id="home" src="webroot/image/home.png" alt="Inicio"></a>
+        </div>
+    </footer>
+</body>
+
+</html>
+
